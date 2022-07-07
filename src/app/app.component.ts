75739578b5e0e0457acc19ed30b98ac2f6f345be
petersongolfredo <102374709+petersongolfredo@users.noreply.github.com>
2022-07-07 16:53:06 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'custom-icon-angular-material';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitzer: DomSanitizer) {
+    
+    this.matIconRegistry.addSvgIcon('customIcon1', this.domSanitzer.bypassSecurityTrustResourceUrl('assets/icons/customIcon1.svg'));
+    this.matIconRegistry.addSvgIcon('customIcon2', this.domSanitzer.bypassSecurityTrustResourceUrl('assets/icons/customIcon2.svg'));
+    this.matIconRegistry.addSvgIcon('customIcon3', this.domSanitzer.bypassSecurityTrustResourceUrl('assets/icons/customIcon3.svg'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
+//TODO: import obrigatorio do 'HttpClientModule' para implementar icones customizados
+import { HttpClientModule } from '@angular/common/http'
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [MatIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
